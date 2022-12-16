dacfacc227e5e738d7d3d6c6332d587f38963483
DolphinBinary <dolphinbinary@gmail.com>
2022-12-16 20:10:25 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*********************************************************************************
+*                                                                                *
+*                        Информационные технологии                               *
+*                                                                                *
+**********************************************************************************
+*   Project name: array-worker       //(laboratory 3)                            *
+*   Project type: ConsoleApplication                                             *
+*   File name: main.cpp                                                          *
+*   Language: CPP                                                                *
+*   Programmers: M3O-111Б-22 Konoplev Klim, Tsoy Dmitriy                         *
+*   Modified by:                                                                 *
+*   Created: 16.12.2022                                                          *
+*   Last revision: 16.12.2022                                                    *
+*   Comment: Одномерные массивы                                                  *
+*********************************************************************************/
+#include <iostream>
+#include <fstream>
+#include "json.h"
+
+int main() {
+    std::ifstream data_file("data.json");
+
+}
